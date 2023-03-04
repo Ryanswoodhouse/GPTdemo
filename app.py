97a36bdd7a9b7523eccb15491304db088f94ea24
HITSZRyan <2582700440ryan@sina.com>
2023-03-04 12:40:01 +0800

    first

--- app.py
+++ app.py
@@ -2,20 +2,20 @@ from flask import Flask, render_template, request
 import openai
 import os
 
-openai.api_key = 'YOUR_API_KEY' # 输入自己的api_key
+openai.api_key = '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' # 输入自己的api_key
 assert(openai.api_key)
 app = Flask(__name__)
 
 messages = []
 @app.route('/')
 def home():
+    
     return render_template('index.html')
 
 @app.route('/get_response', methods=['POST'])
 
 def get_bot_response():
     message = request.form['message']
-    # print(user_input)
     messages.append({'role': 'user', 'content': message})
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -23,8 +23,6 @@ def get_bot_response():
     )
     response = completion.choices[0].message['content']
     print(response)
-    # messages.append({'role': 'assistant', 'content': response})
-    # print(messages)
     return response
 
 if __name__ == '__main__':
